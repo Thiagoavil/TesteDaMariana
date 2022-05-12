7e19bd0fc282d3baac96ecec859658699fff2285
thiag <thiag@NOTETHIAGO>
2022-05-12 19:48:35 -0300

    ..

---
+++ TesteDaMariana.WinAPP/ModuloDisciplina/ConfiguracaoToolBoxDisciplina.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteDaMariana.WinAPP.ModuloDisciplina
+{
+    public class ConfiguracaoToolBoxDisciplina
+    {
+        public override string TipoCadastro => "Controle de Compromissos";
+
+        public override string TooltipInserir => "Inserir um novo compromisso";
+
+        public override string TooltipEditar => "Editar um compromisso existente";
+
+        public override string TooltipExcluir => "Excluir um compromisso existente";
+
+        public override string TooltipFiltrar { get { return "Filtrar Compromisso por Status"; } }
+
+        public override bool FiltrarHabilitado => true;
+    }
+}
--- TesteMariana.Infra.Arquivos/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TesteMariana.Infra.Arquivos
-{
-    public class Class1
-    {
-    }
-}
