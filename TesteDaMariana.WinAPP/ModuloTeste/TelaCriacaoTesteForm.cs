3ef0478b430aad72147522b5cfe07c7145ae572e
thiag <thiag@NOTETHIAGO>
2022-05-15 18:49:09 -0300

    .

--- TesteDaMariana.WinAPP/ModuloTeste/TelaCriacaoTesteForm.cs
+++ TesteDaMariana.WinAPP/ModuloTeste/TelaCriacaoTesteForm.cs
@@ -1,12 +1,6 @@
 ﻿using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using TesteMariana.Dominio.ModuloDisciplina;
 using TesteMariana.Dominio.ModuloMateria;
@@ -18,28 +12,37 @@ namespace TesteDaMariana.WinAPP.ModuloTeste
     public partial class TelaCriacaoTesteForm : Form
     {
         private Teste teste;
-        public TelaCriacaoTesteForm(List<Disciplina>disciplinas)
+        private List<Materia> materias;
+        private List<Questao> questoes;
+        public TelaCriacaoTesteForm(List<Disciplina> disciplinas, List<Materia> materias, List<Questao> questoes)
         {
             InitializeComponent();
+            this.materias = materias;
+            this.questoes = questoes;
             foreach (Questao item in teste.questoes)
             {
                 listBoxQuestoes.Items.Add(item);
             }
             CarregarDisciplinas(disciplinas);
-            var disciplinaselecionada = (Disciplina)comboBoxDisciplina.SelectedItem;
-            CarregarMaterias(disciplinaselecionada);
-            
+
+            CarregarMaterias(materias);
+
         }
 
         public Func<Teste, ValidationResult> GravarRegistro { get; set; }
 
-        private void CarregarMaterias(Disciplina disciplinaSelecionada)
+        private void CarregarMaterias(List<Materia> materias)
         {
+
             comboBoxMateria.Items.Clear();
 
-            foreach (var item in disciplinaSelecionada.materias)
+            foreach (var item in materias)
             {
-                comboBoxMateria.Items.Add(item);
+                if (item.disciplina == (Disciplina)comboBoxDisciplina.SelectedItem)
+                {
+                    comboBoxMateria.Items.Add(item);
+                }
+
             }
         }
 
@@ -68,8 +71,8 @@ namespace TesteDaMariana.WinAPP.ModuloTeste
                 txtData.Text = teste.DataDeCriacao.ToShortDateString();
                 maskedTextBoxQuestoes.Text = teste.QuantidadeDeQuestoes.ToString();
 
-                if(teste.Provao==true)
-                    checkBoxProvão.Checked=true;
+                if (teste.Provao == true)
+                    checkBoxProvão.Checked = true;
                 else
                     checkBoxProvão.Checked = false;
             }
@@ -78,13 +81,13 @@ namespace TesteDaMariana.WinAPP.ModuloTeste
         private void buttonGravar_Click(object sender, EventArgs e)
         {
             teste.Titulo = textBoxTitulo.Text;
-            teste.DataDeCriacao =txtData.Value;
+            teste.DataDeCriacao = txtData.Value;
 
             if (checkBoxProvão.Checked)
-                teste.Provao=true;
+                teste.Provao = true;
             else
-                teste.Provao=false; 
-            
+                teste.Provao = false;
+
 
             var resultadoValidacao = GravarRegistro(teste);
 
@@ -100,15 +103,15 @@ namespace TesteDaMariana.WinAPP.ModuloTeste
 
         private void buttonSortearQuestoes_Click(object sender, EventArgs e)
         {
-            List<Questao> Questoes =new();
+            List<Questao> Questoes = new();
             if (checkBoxProvão.Checked == true)
-               Questoes = CarregarQuestoesProvao();
+                Questoes = CarregarQuestoesProvao();
             else
                 Questoes = CarregarQuestoesMateria();
 
             listBoxQuestoes.Items.Clear();
 
-            foreach(Questao item in Questoes)
+            foreach (Questao item in Questoes)
             {
                 listBoxQuestoes.Items.Add(item);
             }
@@ -116,50 +119,84 @@ namespace TesteDaMariana.WinAPP.ModuloTeste
 
         private List<Questao> CarregarQuestoesProvao()
         {
-            List<Questao> Questoes=new();
+            List<Questao> Questoes = new();
             int numeroDeQuestoes = Convert.ToInt32(maskedTextBoxQuestoes.Text);
             Disciplina disciplinaSelecionada = (Disciplina)comboBoxDisciplina.SelectedItem;
-            
+            List<Questao> sortearQuestão = new();
+
+
             for (int i = 0; i < numeroDeQuestoes; i++)
             {
 
                 //Materia Aleatória
                 Random rnd = new Random();
 
-                Materia materiaselecionada = disciplinaSelecionada.materias[rnd.Next(disciplinaSelecionada.materias.Count)];
+                Materia materiaselecionada = materias[rnd.Next(materias.Count)];
 
+                foreach (Questao item in questoes)
+                {
+                    if (item.materia == materiaselecionada)
+                        sortearQuestão.Add(item);
+                }
 
                 //Questão aleatória
                 Random random = new Random();
 
-                Questao questaoAleatoria = materiaselecionada.questoes[random.Next(materiaselecionada.questoes.Count)];
+                Questao questaoAleatoria = sortearQuestão[random.Next(sortearQuestão.Count)];
 
-                Questoes.Add(questaoAleatoria);
+                if (Questoes.Contains(questaoAleatoria))
+                {
+                    i--;
+                }
+                else
+                {
+                    Questoes.Add(questaoAleatoria);
+                }
 
             }
-           
+
             return Questoes;
         }
-        
+
         private List<Questao> CarregarQuestoesMateria()
         {
-            
+
             int numeroDeQuestoes = Convert.ToInt32(maskedTextBoxQuestoes.Text);
             Materia materiaselecionada = (Materia)comboBoxMateria.SelectedItem;
-            List<Questao> Questoes=new();
+            List<Questao> Questoes = new();
+            List<Questao> sortearQuestão = new();
+
+            foreach (Questao item in questoes)
+            {
+                if (item.materia == materiaselecionada)
+                    sortearQuestão.Add(item);
+            }
 
             //Questões Aleatórias
+
+
             for (int i = 0; i < numeroDeQuestoes; i++)
             {
+
                 Random rnd = new Random();
 
-                Questao questaoAleatoria = materiaselecionada.questoes[rnd.Next(materiaselecionada.questoes.Count)];
+                Questao questaoAleatoria = sortearQuestão[rnd.Next(sortearQuestão.Count)];
 
-                Questoes.Add(questaoAleatoria);
+                if (Questoes.Contains(questaoAleatoria))
+                {
+                    i--;
+                }
+                else
+                {
+                    Questoes.Add(questaoAleatoria);
+                }
+
 
             }
             return Questoes;
         }
+
+
 
     }
 }
